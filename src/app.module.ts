32e875ead8be532cbd233ee44595e684d34534c1
Thomas Bouillon <tbouillon54@gmail.com>
2022-06-18 11:46:19 +0200

    feat: add orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get('DATABASE_HOST'),
+        port: parseInt(config.get('DATABASE_PORT') || '3306'),
+        username: config.get('DATABASE_USER'),
+        password: config.get('DATABASE_PASSWORD'),
+        database: config.get('DATABASE_NAME'),
+        entities: [],
+        synchronize: false,
+      }),
+      inject: [ConfigService],
+      imports: [ConfigModule],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
